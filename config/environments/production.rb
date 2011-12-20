a95a00b91c65c7513bb3fa051e8a9e0ad5350906
Chris Humphreys <humphreys.chris@gmail.com>
2011-12-19 20:42:54 -0800

    Try #2 to fix precompile for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,8 @@ Hnh::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( custom.css )
+  # config.assets.precompile += %w( custom.css blueprint/screen.css )
+  config.assets.precompile = ['*.js', '*.css']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
