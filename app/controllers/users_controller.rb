6370f66ec3d5bf731e595f823263793ac06e5f21
Chris Humphreys <humphreys.chris@gmail.com>
2011-12-22 20:45:52 -0800

    User registration complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,19 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
     @title = "Register"
   end
-
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to Hyp & Hype"
+      redirect_to @user
+    else
+      @title = "Register"
+      @user.password = ""
+      render 'new'
+    end
+  end
 end
