0a0810f1e71b07db0b90dbece161e820adde8bf3
Chris Humphreys <humphreys.chris@gmail.com>
2011-12-23 21:21:06 -0800

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Hnh::Application.routes.draw do
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
   
   match '/register', :to => 'users#new'
-
+  match '/signin',   :to => 'sessions#new'
+  match '/signout',  :to => 'sessions#destroy'
   match '/contact',  :to => 'pages#contact'
   match '/about',    :to => 'pages#about'
   root :to => 'pages#home'  
